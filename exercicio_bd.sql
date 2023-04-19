45276b0c5c8522edc5fe41c9d9eeb1285d31a14e
Rebeca Lôbo <99295582+rebecanlobo@users.noreply.github.com>
2023-04-19 19:51:49 -0300

    Update exercicio_bd.sql

--- exercicio_bd.sql
+++ exercicio_bd.sql
@@ -35,3 +35,68 @@ alter table tbvendas add constraint cdloja foreign key (cdloja) references tbloj
 alter table tbvendas add constraint cdvdd foreign key (cdvdd) references tbloja (cdvdd);
 
 'pg_dump'
+
+
+
+
+
+
+CREATE TABLE TBPRO (
+nmpro integer, 
+cdpro integer primary key,
+tppro integer,
+undpro integer,
+slpro integer,
+stpro integer)
+
+CREATE TABLE TBVEN (
+cdven integer primary key,
+dtven date,
+agecli varchar (3),
+nmcli varchar (100),
+cdcli integer,
+clacli varchar (10),
+sxcli varchar (10) check (sxcli in ('masculino', 'feminino', 'outro')),
+cidcli varchar (100),
+estdcli varchar (3),
+paiscli varchar (100),
+canal varchar (100),
+stven varchar (100),
+cdvdd integer)
+
+CREATE TABLE TBVDD (
+cdvdd integer primary key,
+nmvdd varchar (100),
+sxvdd varchar (10) check (sxvdd in ('masculino', 'feminino', 'outro')),
+percomissao integer,
+matfunc integer,
+idtbven integer)
+
+CREATE TABLE TBVENITEM (
+cdvenitem integer primary key,
+cdpro integer,
+qtven numeric,
+crtven integer,
+vruven integer,
+cdven integer, foreign key (cdven) references TBVEN (cdven))
+
+CREATE TABLE TBDEP (
+cddep integer primary key,
+inepescola varchar (100),
+nmdep varchar (100),
+dtnasc date,
+sxdep varchar (10) check (sxdep in ('masculino', 'feminino', 'outro'))
+)
+
+ALTER TABLE TBVENITEM add constraint cdpro foreign key (cdpro) references TBPRO (cdpro) 
+
+ALTER TABLE TBVEN add constraint cdvdd foreign key (cdvdd) references TBVDD (cdvdd) 
+
+ALTER TABLE TBPRO ALTER COLUMN nmpro type varchar (100)
+
+INSERT INTO TBVEN 
+VALUES (12, '20221202', 21, 'maria', 'fortaleza', 'A', 'Fortaleza', 'CE', 'BR', 'Loja', 12, 158)
+
+select * from tbven
+
+
