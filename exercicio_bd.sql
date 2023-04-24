96b4ca445a32e3b77b48e27119786a24a1bad91e
Rebeca Lôbo <99295582+rebecanlobo@users.noreply.github.com>
2023-04-24 20:00:15 -0300

    Update exercicio_bd.sql

--- exercicio_bd.sql
+++ exercicio_bd.sql
@@ -115,3 +115,54 @@ order by cdvdd desc
 
 SELECT cdpro as codigoproduto, nmpro as nomeproduto from tbpro where cdpro is not null and
 tbpro is not null group by cdpro, nmpro where dtven between 2014-02-03 to 2018-02-02
+
+
+
+
+
+
+
+
+
+
+-- Questão 1: Apresente a query para listar o código e 
+-- o nome do vendedor com maior número de vendas (contagem) e que estas vendas estejam com status concluído.
+
+SELECT cdvdd as codigovendedor, nmvdd as nomevendedor from tbvdd where cdvdd is not null and nmvdd is not null
+group by cdvdd, nmvdd
+order by cdvdd desc
+
+-- Questão 2: Apresente a query para listar o código e o nome do produto mais vendido entre as datas 
+-- 2014-02-03 até 2018-02-02.
+
+SELECT cdpro as codigoproduto, nmpro as nomeproduto, dtven
+from tbven
+join tbpro on cdpro = cdpro
+where dtven between '2014-02-03' and '2018-02-02'
+
+-- Questão 3: Apresente a query para listar o código e o nome do cliente com maior gasto na loja.
+-- As colunas presentes no resultado devem ser codigocliente (cdcli), nomecliente (nmcli),
+-- e gasto, esta última representando o somatório das vendas atribuídas ao cliente.
+
+SELECT cdcli as codigocliente, nmcli as nomecliente, 
+SUM(vrtven) as gasto
+from tbven
+join tbven_item on tbven.cdcli = tbven.cdcli
+group by cdcli, nmcli
+
+-- Questão 4: Apresente a query para listar o código, nome e data de nascimento dos dependentes
+-- do vendedor com menor valor total bruto em vendas (não sendo zero). As colunas presentes no documento
+-- devem ser codigodependente (cddep), nomedependente (nmdep), datanascimento (dtnasc).
+
+SELECT cddep as codigodependente, nmdep as nomedependente, dtnasc as datanascimento,
+from tbdep where 
+join tbdep on cddep = cddep
+
+-- Questão 5: Apresente a query para listar os 3 produtos menos vendidos pelos canais de e-commerce
+-- ou matriz. As colunas presentes devem ser canalvendas (canal), codigoproduto (cdpro), nomeproduto (nmpro),
+-- e quantidade_vendas.
+
+SELECT cdpro as codigoproduto, nmpro as nomeproduto, canal as canalvendas, sum(qtven) as quantidade_vendas
+from tbven
+join tbpro on tbven.cdpro = tbpro.cdpro
+
